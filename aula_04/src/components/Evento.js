651a93ec3c1ca0bf9a53024ccfb91ea462bd00e2
lordeiro-dias <eduborgesbldias@gmail.com>
2022-09-26 08:29:06 -0300

    26-09

---
+++ aula_04/src/components/Evento.js
@@ -0,0 +1,14 @@
+export default function Evento({palavra}){
+
+    function meuEvento(){
+        console.log(`Opa! Fui Ativado! ${palavra}`)
+    }
+
+    return(
+        <div>
+            <p>Clique para disparar um evento</p>
+            <button onClick={meuEvento}>ATIVAR!</button>
+        </div>
+    )
+
+}
